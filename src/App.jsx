75449d8a2b715972d3efe8468c577304f4da9225
KélyanDev <therealmortem@gmail.com>
2024-03-29 09:26:51 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HeaderNav from './Components/HeaderNav';
+import ManageTasks from './Components/ManageTasks';
+import Footer from './Components/Footer';
+
+export default function App() {
+    return (
+      <>
+        <HeaderNav />
+        <ManageTasks />
+        <Footer />
+      </>
+      );
+};
